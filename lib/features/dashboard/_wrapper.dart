540ef53822b8407cd2157cee2b8060199c45f49c
Kaium Al Limon <limon.3cs@gmail.com>
2026-03-12 16:20:36 +0600

    add DashboardWrapper widget for the dashboard feature

---
+++ lib/features/dashboard/_wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DashboardWrapper extends StatelessWidget {
+  const DashboardWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
